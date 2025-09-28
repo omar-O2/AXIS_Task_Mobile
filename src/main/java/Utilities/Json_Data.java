039f40081d7b6433d0b46cb69a0be5f13c5daa94
o.radwan <o.radwan@tammwel.com>
2025-09-28 10:18:53 +0300

    Updates json

--- src/main/java/Utilities/Json_Data.java
+++ src/main/java/Utilities/Json_Data.java
@@ -14,36 +14,6 @@ public class Json_Data {
 
 
 
-    /*public static String Get_json_Data(String File_Name, String Field_Name) throws FileNotFoundException {
-        FileReader reader = new FileReader(Path_Json_File+File_Name+".json");
-        JsonElement jsonElement = JsonParser.parseReader(reader);
-        return jsonElement.getAsJsonObject().get(Field_Name).getAsString();
-    }*/
-   /*public static String Get_json_Data(String File_Name, String Field_Path) throws FileNotFoundException {
-       FileReader reader = new FileReader(Path_Json_File + File_Name + ".json");
-       JsonElement jsonElement = JsonParser.parseReader(reader);
-       JsonObject currentObj = jsonElement.getAsJsonObject();
-
-       // Split path (e.g., "User1.NationalID1" → ["User1", "NationalID1"])
-       String[] pathParts = Field_Path.split("\\.");
-
-       // Traverse nested objects
-       for (int i = 0; i < pathParts.length - 1; i++) {
-           currentObj = currentObj.getAsJsonObject(pathParts[i]);
-           if (currentObj == null) {
-               throw new RuntimeException("Path not found: " + Field_Path);
-           }
-       }
-
-       // Get the final field value
-       JsonElement fieldValue = currentObj.get(pathParts[pathParts.length - 1]);
-       if (fieldValue == null || fieldValue.isJsonNull()) {
-           throw new RuntimeException("Field not found: " + Field_Path);
-       }
-       return fieldValue.getAsString();
-   }*/
-
-
         public static String Get_json_Data(String File_Name, String Field_Name) {
             try (FileReader reader = new FileReader(Path_Json_File + File_Name + ".json")) {
                 JsonElement element = JsonParser.parseReader(reader);
